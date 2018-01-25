e6ec9aa3dcf542f7d8474a64c374d51f716adc07
ai-banzai <mikroself@gmail.com>
2018-01-26 00:38:51 +0100

    end project started

--- project-pixel-art-maker-starter-master/designs.js
+++ project-pixel-art-maker-starter-master/designs.js
@@ -1,10 +1,25 @@
 // Select color input
+let color="#000000";
+$("#colorPicker").change(function(){
+  color=this.value;
+})
+
 // Select size input
+let height=1;
+$("#inputHeight").change(function(){
+  height=this.value;
+})
+
+let width=1;
+$("#inputWidth").change(function(){
+  width=this.value
+})
 
 // When size is submitted by the user, call makeGrid()
+$("#submit").click(function(){makeGrid()});
 
 function makeGrid() {
 
 // Your code goes here!
-
+console.log("submitted: " + height + ", " + width);
 }
