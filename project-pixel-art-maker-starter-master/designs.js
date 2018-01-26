e28f8843d00887928508cf333389027f6f251f7d
ai-banzai <mikroself@gmail.com>
2018-01-26 01:19:15 +0100

    final project completed

--- project-pixel-art-maker-starter-master/designs.js
+++ project-pixel-art-maker-starter-master/designs.js
@@ -16,10 +16,26 @@ $("#inputWidth").change(function(){
 })
 
 // When size is submitted by the user, call makeGrid()
-$("#submit").click(function(){makeGrid()});
+$("#submit").click(function(e){
+  e.preventDefault();
+  makeGrid()});
 
 function makeGrid() {
 
-// Your code goes here!
-console.log("submitted: " + height + ", " + width);
-}
+// create table of appropriate size
+  console.log("submitted: " + height + ", " + width);
+  $("#pixelCanvas").children().remove();
+  let tableData = "";
+  for (var c = 0; c < width; c++) {
+    tableData = tableData + "<td></td>";
+  };
+  let tableRow = "<tr>" + tableData + "</tr>";
+  for (var r = 0; r < height; r++) {
+    $("#pixelCanvas").append(tableRow);
+  };
+};
+
+// add event listener on click to every cell
+$("#pixelCanvas").click("td", function(evt){
+  $(evt.target).css("background-color", color);
+});
